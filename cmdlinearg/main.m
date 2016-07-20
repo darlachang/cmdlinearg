6d1c1fbe71a064bbb31a6d6db11a01808b91e10c
MingHan <sweetteaoh@gmail.com>
2016-07-20 13:42:06 +0800

    second commit

--- cmdlinearg/main.m
+++ cmdlinearg/main.m
@@ -10,8 +10,35 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        struct entry dictionary[100] = {
+            { "aardvark", "a burrowing African mammal" },
+            { "abyss", "ueslkr", "sersrlk"},
+            { "tjeoj", "serioj", "serosj"},
+            { "re;orij", "serlselr", "sfo9g"},
+            { "ser", "seroj", "93rl"},
+            { "liqe", "tet9s", "3rs"},
+            { "sdf", "sdflke", "sdf"},
+            { "qwe", "asd;o9", "f2t"},
+            { "gower", "sdgo;ie", "sdfx"},
+            { "er;oi", "sdf3", "serc"}
+        };
+        
+        int entries = 10;
+        int entryNumber;
+        int lookup (struct entry dictionary [], char search[], int entries);
+        
+        if ( argc != 2)
+        {
+            NSLog(@"No word typed on the command line.");
+            return (1);
+        }
+        
+        entryNumber = lookup (dictionary, argv[1], entries);
+        if ( entryNumber != -1)
+            NSLog(@"%s", dictionary[entryNumber].definition);
+        else
+            NSLog (@"Sorry, %s is not in my dictionary", argv[1]);
     }
     return 0;
 }
